f405f33720efc885cc7be400317845f23f5a00a3
Bailey <734019@newcollege.ac.uk>
2025-10-13 11:10:25 +0100

    added a teleporter

---
+++ Assets/TELEPORTER.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TELEPORTER : MonoBehaviour
+{
+
+    public Transform player;
+    public Transform destination;
+    public GameObject playerg;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerg.SetActive(false);
+
+            player.position = destination.position;
+
+            playerg.SetActive(true);
+        }
+    }
+
+}
