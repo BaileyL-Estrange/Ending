26f9766be2e4dd96af2154b0936ee87f48d39148
Bailey <734019@newcollege.ac.uk>
2025-11-05 10:31:00 +0000

    kill

--- Assets/KillPlayer.cs
+++ Assets/KillPlayer.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class KillPlayer : MonoBehaviour
 {
@@ -12,6 +13,8 @@ public class KillPlayer : MonoBehaviour
             Debug.Log("Player died!");
 
             Destroy(collision.gameObject);
+
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
     }
 }
